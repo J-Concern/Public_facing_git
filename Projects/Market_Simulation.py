780f80d153f8ab979236f7948808532ae6c227bb
J-Concern <137921155+J-Concern@users.noreply.github.com>
2025-05-05 22:07:20 -0400

    Update Market_Simulation.py

--- Projects/Market_Simulation.py
+++ Projects/Market_Simulation.py
@@ -1,43 +1,54 @@
 import random
+from matplotlib import pyplot as plt
 
 #obtain starting variables
 expected_interest_rate = float(0.07)
 principle_investment = float(100000)
+current_investment = principle_investment
 time_of_investment = float(15)
-variance_high = 0.20
-variance_low = 0.15
+variance_high = .2
+variance_low = .15
+outcome_low = 0
+outcome_high = 0
 
-def expected_outcome(expected_interest_rate, principle_investment):
-    outcome = int(((expected_interest_rate * principle_investment) + principle_investment))
-    ytd_return = int(expected_interest_rate * principle_investment)
-    print("$ ", outcome)
-    return outcome, ytd_return
+#function to obtain yearly baseline monetary output and year to date return
+def expected_outcome(expected_interest_rate, current_investment):
+    interest_gained = expected_interest_rate * current_investment
+    current_investment = int(current_investment + interest_gained)
+    print("$:",current_investment)
+    return current_investment
 
-    
-def variance_in_outcome(outcome, variance_high, variance_low):
-    n = 0
+#function to find a variance from a random number given
+def variance_in_outcome(variance_high, variance_low, outcome_high, outcome_low, principle_investment, expected_interest_rate, n = False):
     variance = random.uniform(variance_low, variance_high)
-    print("Random Variance:" ,variance)
-    altered_outcome_high = int((outcome * variance) + outcome)
-    altered_outcome_low =  int(outcome - (outcome * variance))
-    if n == 1:
-        altered_outcome_high = altered_outcome_high + (outcome + (altered_outcome_high * variance))
-        altered_outcome_low = altered_outcome_low + (outcome + (altered_outcome_high * variance))
-    n = n + 1
-    return altered_outcome_high, altered_outcome_low
+    outcome_modifier_low = expected_interest_rate - (expected_interest_rate *variance)
+    outcome_modifier_high = expected_interest_rate + (expected_interest_rate * variance)
+    if n == False:
+        outcome_low = outcome_modifier_low * principle_investment
+        outcome_high = outcome_modifier_high * principle_investment
+        n = True
+        return outcome_high, outcome_low
+    outcome_high = outcome_high +(outcome_high * outcome_modifier_high)
+    outcome_low = outcome_low + (outcome_low * outcome_modifier_low)
+    return outcome_high, outcome_low
 
-#def graph():
 
-def main(time_of_investment):
-    for n in range(0,int(time_of_investment)):
-        outcome, ytd_return = expected_outcome(expected_interest_rate, principle_investment)
-        altered_outcome_high, altered_outcome_low = variance_in_outcome(outcome, variance_high, variance_low)
-        n += 1
-        print("High $:",altered_outcome_high, "Low $:", altered_outcome_low)
-        outcome = outcome + ytd_return
-        print(outcome)
+outcome_high, outcome_low = variance_in_outcome(variance_high, variance_low, outcome_high, outcome_low, principle_investment, expected_interest_rate)
 
+variance_in_outcome(variance_high, variance_low, outcome_high, outcome_low, principle_investment, expected_interest_rate)
+
+
+
+
+#main loop
+def main(time_of_investment, n=0):
+   outcome_high, outcome_low = variance_in_outcome(variance_high, variance_low, outcome_high, outcome_low, principle_investment, expected_interest_rate)
+   current_investment = expected_outcome(expected_interest_rate, current_investment)
+   while n < time_of_investment:
+    print(outcome_high, outcome_low)
+    print(current_investment)
+    n=n+1
 
 
 if __name__ == "__main__":
-    main(time_of_investment)
+    main(time_of_investment, n=0)
