7486eb4b8ccc3e9b44530e2bc10cbcaf0760004d
J-Concern <137921155+J-Concern@users.noreply.github.com>
2025-05-27 00:16:15 -0400

    quick_sort project

---
+++ Projects/quick_sort.py
@@ -0,0 +1,78 @@
+import random
+import math
+
+def is_sorted(array, ascending = True):
+    #helper function, can measure if an array is sorted in O(n) worst case.
+    for i in range(0,int(len(array)-1)):
+        #iterates through the array
+        if ascending == True:
+            #for ascending cases compares index with its right neighbor
+            if array[i] > array[i+1]:
+                return False
+        if ascending == False:
+            #for desceding cases compares index with left neighbor
+            if array[i] < array[i+1]:
+                return False
+    #if iterates through with no False returns then returns true. 
+    return True
+
+def number_sort_algo(array, ascending = True):
+    # this does the work of sorting in best case O n log(n) time. 
+    if len(array) <= 2:
+        #removes edge cases of array being to small, if True returns array.
+        print("Array too Short.")
+        return array
+    
+    # initates a step counter to measure efficiency.
+    step_counter = 0
+
+    while is_sorted(array) == False:
+        # this while loop runs until is_sorted returns true.
+    
+        #increments the step counter by 1
+        step_counter += 1
+
+        #determines a random pivot and gets the index.
+        pivot_index = math.floor(random.randint(0, (len(array)-1)))
+        pivot = array[pivot_index]
+        
+        #creates arrays to store the wings of data.
+        less_than = []
+        greater_than = []
+
+        if ascending == True:
+            #for ascending iterates through and appends greater and less than.
+            for i in array:
+                if i <= pivot:
+                    less_than.append(i)
+                else:
+                    greater_than.append(i)
+
+        if ascending == False:
+            #for ascending iterates through and appends greater and less than.
+            for i in array:
+                if i >= pivot:
+                    less_than.append(i)
+                else:
+                    greater_than.append(i)
+        array = less_than + greater_than
+
+    if is_sorted(array) == True:
+        #checks if array is sorted, if True returns array.
+        print("Returned sorted Array.")
+        return array, step_counter
+    
+    else:
+        #error control, if something fails this should be able to pass False
+        print("Error")
+        return False
+                
+
+def sort_algo_test(array_size):
+    #a small test function
+    array = [random.randint(1,1000) for i in range(array_size)]
+    array, step_counter = number_sort_algo(array)
+    print(array)
+    print(step_counter)
+
+sort_algo_test(400)
